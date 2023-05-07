07d3897ed78ade22180364ec7a976f9762b40844
Mykyta Levskyi <nikita.levskiy@gmail.com>
2023-05-07 15:36:47 +0000

    Making migrations to the database

--- footballPrj/footballApp/models.py
+++ footballPrj/footballApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    firstTeam = models.CharField(max_length=20, blank=False)
+    firstTeamStatus = models.CharField(max_length=5, blank=False)
+    goalsFirstTeam = models.IntegerField(default=0)
+    playersFirstTeam = models.CharField(max_length=300, blank=False)
+
+    secondTeam = models.CharField(max_length=20, blank=False)
+    secondTeamStatus = models.CharField(max_length=5, blank=False)
+    goalsSecondTeam = models.IntegerField(default=0)
+    playersSecondTeam = models.CharField(max_length=300, blank=False)
+
+    eventOwner = models.CharField(max_length=20, blank=False)
+    eventStatus = models.CharField(max_length=20, blank=False)
+
+    def get_absolute_url(self):
+        return '/'
+
+    def __str__(self):
+        return self.firstTeam + " - " + self.secondTeam
+
+
+class Team(models.Model):
+    place = models.CharField(max_length=3, blank=False)
+    teamName = models.CharField(max_length=20, blank=False)
+    win = models.CharField(max_length=3, blank=False)
+    loss = models.CharField(max_length=3, blank=False)
+    draw = models.CharField(max_length=3, blank=False)
+
+    def __str__(self):
+        return self.teamName
