3f16eb82efb08fab06d8312ef6a035f1f4d7bfa2
Mykyta Levskyi <nikita.levskiy@gmail.com>
2023-05-07 16:43:14 +0000

    User login form added

--- footballPrj/footballPrj/urls.py
+++ footballPrj/footballPrj/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from footballApp.views import indexPage, registrationPage
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', indexPage, name="index"),
-    path('registration/', registrationPage, name="registration")
+    path('registration/', registrationPage, name="registration"),
+    path('', include('django.contrib.auth.urls')),
 ]
